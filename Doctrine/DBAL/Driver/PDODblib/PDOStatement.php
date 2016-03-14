95ef1b88d8deffa3a7ef22cf01b301956d2afbf3
Roel Arents <r.arents@bva-auctions.com>
2016-03-14 20:53:13 +0100

    fix null character should also be filtered

--- Doctrine/DBAL/Driver/PDODblib/PDOStatement.php
+++ Doctrine/DBAL/Driver/PDODblib/PDOStatement.php
@@ -4,33 +4,36 @@ namespace Lsw\DoctrinePdoDblib\Doctrine\DBAL\Driver\PDODblib;
 
 class PDOStatement extends \Doctrine\DBAL\Driver\PDOStatement
 {
-    
+    /**
+     * @var string Regex matching freetds non-supported characters.
+     */
+    const FREETDS_INVALID_CHAR_REGEX = '/[^\x{1}-\x{FFFF}]/u';
+
     /**
      * {@inheritdoc}
-     * 
+     *
      * Freetds communicates with the server using UCS-2 (since v7.0).
-     * Freetds claims to convert from any given client charset to UCS-2 using iconv. Which should strip or replace unsupported chars. 
+     * Freetds claims to convert from any given client charset to UCS-2 using iconv. Which should strip or replace unsupported chars.
      * However in my experience, characters like 👍 (THUMBS UP SIGN, \u1F44D) still end up in Sqlsrv shouting '102 incorrect syntax'.
+     * As does the null character \u0000.
+     *
      * Upon binding a value, this function replaces the unsupported characters.
      */
     public function bindValue($param, $value, $type = \PDO::PARAM_STR)
     {
         if ($type == \PDO::PARAM_STR) {
-            $value = static::replaceNonUcs2Chars($value);
+            $value = static::replaceUnsupportedFreetdsChars($value);
         }
         return parent::bindValue($param, $value, $type);
     }
 
     /**
-     * UCS-2 cannot represent Unicode code points outside the BMP. (> 16 bits.)
-     * This function replaces those characters in a string with the REPLACEMENT CHARACTER.
+     * This function replaces characters in a string unsupported by freetds with the REPLACEMENT CHARACTER.
      * @param string $val
      * @return string
      */
-    public static function replaceNonUcs2Chars($val)
+    public static function replaceUnsupportedFreetdsChars($val)
     {
-        return is_string($val) ? \preg_replace('/[^\x{0}-\x{FFFF}]/u', "�", $val) : $val;
+        return is_string($val) ? \preg_replace(static::FREETDS_INVALID_CHAR_REGEX, "�", $val) : $val;
     }
-    
-    
 }
